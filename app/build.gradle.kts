5acc7bfccf683f9346323cdc07c8c2cba454a79f
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-12 17:34:11 +0100

    Nueva dependencia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
