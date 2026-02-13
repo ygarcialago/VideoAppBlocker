c1f9efeeb98c6c8d94a1d882e907d33b14bbbeee
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-13 12:03:07 +0100

    Cambio de función a corrutina.

--- app/src/main/java/com/example/videoappblocker/viewmodel/AppListViewModel.kt
+++ app/src/main/java/com/example/videoappblocker/viewmodel/AppListViewModel.kt
@@ -1,28 +1,47 @@
 package com.example.videoappblocker.viewmodel
 
 import android.app.Application
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.application
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AppListViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _apps = mutableStateOf<List<String>>(emptyList())
     val apps: State<List<String>> = _apps
+    private val _isLoading = mutableStateOf(false)
+    val isLoading: State<Boolean> = _isLoading
 
-    fun loadInstalledApps() {
-        val pm = getApplication<Application>().packageManager
-        val apps = pm.getInstalledApplications(0)
+    fun loadInstalledApps(onFinished: () -> Unit = {}) {
+        if (_isLoading.value) return
 
-        val userApps = apps
-            .filter { it.flags and ApplicationInfo.FLAG_SYSTEM == 0 }
-            .map { pm.getApplicationLabel(it).toString() }
+        viewModelScope.launch {
+            _isLoading.value = true
 
-        _apps.value = userApps
+            val result = withContext(Dispatchers.IO) {
+                val pm = getApplication<Application>().packageManager
+                val allApps = pm.getInstalledApplications(PackageManager.GET_META_DATA)
+
+                Log.d("AppCheck", "Total: ${allApps.size}")
+
+                allApps.filter { appInfo ->
+                    pm.getLaunchIntentForPackage(appInfo.packageName) != null
+                }.map { appInfo ->
+                    pm.getApplicationLabel(appInfo).toString()
+                }.sortedBy { it.lowercase() }
+            }
+
+            _apps.value = result
+            _isLoading.value = false
+
+            onFinished()
+        }
     }
 }
 
