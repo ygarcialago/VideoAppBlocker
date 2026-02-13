dec9eca3a172ac5e6c7f71b9edb8165dfaa71ca4
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-13 10:54:11 +0100

    Cambio en la clase. En vez de devolver las apps en una lista, actualiza un estado de la clase

--- app/src/main/java/com/example/videoappblocker/viewmodel/AppListViewModel.kt
+++ app/src/main/java/com/example/videoappblocker/viewmodel/AppListViewModel.kt
@@ -3,24 +3,26 @@ package com.example.videoappblocker.viewmodel
 import android.app.Application
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.application
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 
 class AppListViewModel(application: Application) : AndroidViewModel(application) {
 
-    fun getInstalledUserApps(): List<String> {
-        val pm = application.packageManager
-        val apps = pm.getInstalledApplications(PackageManager.GET_META_DATA)
-        val installedAppsNames = mutableListOf<String>()
+    private val _apps = mutableStateOf<List<String>>(emptyList())
+    val apps: State<List<String>> = _apps
 
-        for (app in apps) {
-            if (app.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
-                val label = pm.getApplicationLabel(app).toString()
-                installedAppsNames.add(label)
-            }
-        }
-        return installedAppsNames
+    fun loadInstalledApps() {
+        val pm = getApplication<Application>().packageManager
+        val apps = pm.getInstalledApplications(0)
+
+        val userApps = apps
+            .filter { it.flags and ApplicationInfo.FLAG_SYSTEM == 0 }
+            .map { pm.getApplicationLabel(it).toString() }
+
+        _apps.value = userApps
     }
 }
 
