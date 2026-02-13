93c8abb81cc08f931b3f0bf384e5db9898c3bb44
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-13 12:06:02 +0100

    ViewModel declarado y enviado a las composables + navController declarado para navegaciones

--- app/src/main/java/com/example/videoappblocker/MainActivity.kt
+++ app/src/main/java/com/example/videoappblocker/MainActivity.kt
@@ -8,8 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.videoappblocker.ui.screens.UI
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.videoappblocker.ui.screens.AppListWindow
+import com.example.videoappblocker.ui.screens.MainWindow
 import com.example.videoappblocker.ui.theme.VideoAppBlockerTheme
+import com.example.videoappblocker.viewmodel.AppListViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +23,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VideoAppBlockerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    // Aplicamos el padding a toda la UI
-                    Modifier.padding(innerPadding)
-                    UI()
+
+                val navController = rememberNavController()
+                val viewModel: AppListViewModel = viewModel()
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "main",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+
+                        composable("main") {
+                            MainWindow(navController, viewModel)
+                        }
+
+                        composable("app_list") {
+                            AppListWindow(navController, viewModel)
+                        }
+                    }
                 }
             }
         }
