16b69845365ee67ff0975907d7c2445039344a40
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-16 14:45:57 +0100

    ViewModel + view para la selección del vídeo

---
+++ app/src/main/java/com/example/videoappblocker/ui/components/ChoseVideo.kt
@@ -0,0 +1,56 @@
+package com.example.videoappblocker.ui.components
+
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import com.example.videoappblocker.viewmodel.VideoSettingsViewModel
+
+@Composable
+fun VideoChose(viewModel: VideoSettingsViewModel) {
+
+    val context = LocalContext.current
+    val selectedVideoUri by viewModel.selectedVideoUri
+
+    LaunchedEffect(Unit) {
+        viewModel.loadSavedVideo()
+    }
+
+    val videoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.OpenDocument()
+    ) { uri: Uri? ->
+
+        uri?.let {
+
+            // 🔥 CLAVE PARA PERSISTENCIA REAL
+            context.contentResolver.takePersistableUriPermission(
+                it,
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+            )
+
+            viewModel.saveVideoUri(it)
+        }
+    }
+    val fileName = selectedVideoUri?.let {
+        viewModel.getFileName(context, it)
+    }
+
+
+    Button(
+            onClick = {
+                videoPickerLauncher.launch(arrayOf("video/*"))
+            }
+        ) {
+
+            Text(fileName ?: "Selecciona un vídeo")
+        }
+
+}
+
