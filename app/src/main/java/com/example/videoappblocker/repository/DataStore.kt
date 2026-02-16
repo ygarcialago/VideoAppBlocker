4782e07762e48b37f94f92869464cfe531adbe15
yoelgarcialago <yoelgarcialago@gmail.com>
2026-02-16 14:46:29 +0100

    Variables + imports no usados borrados

--- app/src/main/java/com/example/videoappblocker/repository/DataStore.kt
+++ app/src/main/java/com/example/videoappblocker/repository/DataStore.kt
@@ -1,8 +1,6 @@
 package com.example.videoappblocker.repository
 
 import android.content.Context
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.stringSetPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 
 val Context.dataStore by preferencesDataStore(name = "blocked_apps")
--- app/src/main/java/com/example/videoappblocker/ui/components/Timer.kt
+++ app/src/main/java/com/example/videoappblocker/ui/components/Timer.kt
@@ -4,11 +4,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -29,8 +26,6 @@ fun TimerScreen(viewModel: TimerViewModel = viewModel()) {
 
     val timeLeft by viewModel.timeLeft
     val isRunning by viewModel.isRunning
-
-    var inputTime by remember { mutableStateOf("") }
 
     var hours by remember { mutableStateOf("") }
     var minutes by remember { mutableStateOf("") }
