40946006212d27b04166e7f545d40fdd5b1b2cea
yoelgarcialago <yoelgarcialago@gmail.com>
2026-03-13 16:38:09 +0100

    TimeInput para mejor UI/UX

--- app/src/main/java/com/example/videoappblocker/ui/components/Timer.kt
+++ app/src/main/java/com/example/videoappblocker/ui/components/Timer.kt
@@ -2,16 +2,22 @@ package com.example.videoappblocker.ui.components
 
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TimeInput
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.videoappblocker.viewmodel.TimerViewModel
@@ -41,66 +48,11 @@ fun TimerScreen(viewModel: TimerViewModel) {
             text = "Tiempo restante: ${viewModel.formattedTime}",
             style = MaterialTheme.typography.headlineMedium
         )
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
-            OutlinedTextField(
-                modifier = Modifier.weight(1f),
-                value = hours,
-                onValueChange = { hours = it.filter { c -> c.isDigit() } },
-                label = { Text("HH") },
-                singleLine = true
-            )
-
-            OutlinedTextField(
-                modifier = Modifier.weight(1f),
-                value = minutes,
-                onValueChange = { minutes = it.filter { c -> c.isDigit() } },
-                label = { Text("MM") },
-                singleLine = true
-            )
-
-            OutlinedTextField(
-                modifier = Modifier.weight(1f),
-                value = seconds,
-                onValueChange = { seconds = it.filter { c -> c.isDigit() } },
-                label = { Text("SS") },
-                singleLine = true
-            )
-        }
         Spacer(modifier = Modifier.height(16.dp))
 
+        TimerPicker(viewModel, isRunning)
 
         Spacer(modifier = Modifier.height(16.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            Button(
-                onClick = {
-                    val h = hours.toIntOrNull() ?: 0
-                    val m = (minutes.toIntOrNull() ?: 0).coerceIn(0, 59)
-                    val s = (seconds.toIntOrNull() ?: 0).coerceIn(0, 59)
-
-                    val totalSeconds = h * 3600 + m * 60 + s
-                    viewModel.startTimer(totalSeconds)
-                },
-                enabled = !isRunning
-            ) {
-                Text("Start")
-            }
-
-            Button(
-                onClick = { viewModel.resetTimer() },
-                enabled = !(viewModel.strictMode.value && isRunning)
-
-            ) {
-                Text("Stop")
-            }
-        }
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -110,6 +62,47 @@ fun TimerScreen(viewModel: TimerViewModel) {
                 enabled = !isRunning
             )
             Text("Bloquear Stop")
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TimerPicker(viewModel: TimerViewModel, isRunning: Boolean) {
+
+    val state = rememberTimePickerState(
+        initialHour = 0,
+        initialMinute = 0,
+        is24Hour = true
+    )
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            TimeInput(
+                state = state
+            )
+        }
+        Spacer(Modifier.height(8.dp))
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(onClick = {
+                val totalSeconds = state.hour * 3600 + state.minute * 60
+                viewModel.startTimer(totalSeconds)
+            }) {
+                Text("Start")
+            }
+            Button(
+                onClick = { viewModel.resetTimer() },
+                enabled = !(viewModel.strictMode.value && isRunning)
+
+            ) {
+                Text("Stop")
+            }
         }
 
     }
